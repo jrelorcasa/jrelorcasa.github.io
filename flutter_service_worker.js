e11b34bb785a5adaf925e8740c8738cd47b4659f
Jasmin Relorcasa <60377986+jrelorcasa@users.noreply.github.com>
2022-06-29 03:22:24 +0800

    Add static fields to Personalize Screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a92b9b8aee148c9af9dee62dc6b8e6ac",
-"index.html": "02cae65f8af7be12e22169bd985f7799",
-"/": "02cae65f8af7be12e22169bd985f7799",
-"main.dart.js": "8212b2e51502d535d0758d18a10f6bd8",
+"index.html": "8b499c54f773591d79a1dd89c0a181c6",
+"/": "8b499c54f773591d79a1dd89c0a181c6",
+"main.dart.js": "36398c9ae4d5005dc459f73c265fc39b",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
