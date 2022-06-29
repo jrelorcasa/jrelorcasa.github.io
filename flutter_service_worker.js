bb67a40a5462352a0793e0272ef86e51800305e7
Jasmin Relorcasa <60377986+jrelorcasa@users.noreply.github.com>
2022-06-29 21:04:34 +0800

    Remove capture email screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a92b9b8aee148c9af9dee62dc6b8e6ac",
-"index.html": "13ede33603723547996328f5a8b6c55a",
-"/": "13ede33603723547996328f5a8b6c55a",
-"main.dart.js": "57f2d76c4e6cea02cfa92d2dcc6bd5d5",
+"index.html": "ce0520d9cd14d98c396ba8f9206172e7",
+"/": "ce0520d9cd14d98c396ba8f9206172e7",
+"main.dart.js": "5ad76a92d6e665900ba807d8e6adb546",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
